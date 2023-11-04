32972b95ce1e57f57cddb3814ebe562a87f64c89
Ayisha06 <147239720+Ayisha06@users.noreply.github.com>
2023-11-04 15:16:19 +0530

    04nov2023

---
+++ demo.py
@@ -0,0 +1,9 @@
+myset1={1,2,3,4,5}
+print(myset1)
+
+first_name='Ayisha'
+last_name='Hasina'
+print('My first name is {} and last name is {}'.format(first_name,last_name))
+
+len('Ayisha')
+print(len)
